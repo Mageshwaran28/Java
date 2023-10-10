e9515226b207edde95b0ff08fb5c89dc8ca9ce85
Mageshwaran <123348992+Mageshwaran28@users.noreply.github.com>
2023-10-10 11:11:24 +0530

    Completed Updates

--- Tree.java
+++ Tree.java
@@ -1,17 +1,38 @@
+
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Tree {
     public static void main(String[] args) {
-        int[][] tree = { { 2, 4 }, { 3, 5 }, { 5, 2 }, { 6, 3 }, { 8, 4 } };
-        if (tree[0][1] == tree[1][1]) {
-            System.out.println("False");
-            return;
-        }
-        for (int i = 2; i < tree.length; i++) {
-            if (tree[i][1] != tree[i - 2][0] && tree[i][1] != tree[i - 1][0]) {
-                System.out.println("False");
-                return;
-            }
-        }
-        System.out.println("True");
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of pairs");
+        int numberOfPairs = scanner.nextInt();
+
+        int[][] tree = new int[numberOfPairs][2];
+        HashMap<Integer, Integer> treeMap = new HashMap<Integer, Integer>();
+        
+        for (int i = 0; i < numberOfPairs; i++) {
+
+            tree[i][0] = scanner.nextInt();
+            tree[i][1] = scanner.nextInt();
+
+            if(!treeMap.containsKey(tree[i][1])) {
+                treeMap.put(tree[i][1], 1);
+            }
+            else{
+                int value = treeMap.get(tree[i][1])+1;
+                if(value>2){
+                    System.out.println("False");
+                    scanner.close();
+                    return;
+                }
+                treeMap.put(tree[i][1],value);
+            }
+
+        }
+        
+        scanner.close();
+        System.out.println("True");
     }
 }
