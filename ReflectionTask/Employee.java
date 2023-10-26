1adf5bf75d64e52791669ae698c87825595a9dca
Mageshwaran28 <mageshwaranbalaji25@gmail.com>
2023-10-26 18:15:15 +0530

    Update in generic way

---
+++ ReflectionTask/Employee.java
@@ -0,0 +1,13 @@
+package ReflectionTask;
+
+public class Employee{
+    float harsha;
+    boolean karthick;
+    int paul;
+    String kishor;
+    String vignesh;
+    String riyan;
+    String magesh;
+    long kirthika;
+    String ajay;
+}
---
+++ ReflectionTask/Student.java
@@ -0,0 +1,13 @@
+package ReflectionTask;
+
+public class Student {
+    long ajay = 28l;
+    int json = 108;
+    String magesh = "I'm a student";
+    int riyan = 100;
+    float vignesh = 35f;
+    String paul = "300";
+    String harsha = "2";
+    boolean kishor = false;
+    String karthick = "true";
+}
