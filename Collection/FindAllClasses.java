77710aa0cfafb74bc9ac3ece743ff87ca4d3dec1
Mageshwaran28 <mageshwaranbalaji25@gmail.com>
2023-10-30 15:01:29 +0530

    Get the all the class within the given package

---
+++ Collection/FindAllClasses.java
@@ -0,0 +1,53 @@
+package Collection;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+public class FindAllClasses {
+
+    public static List<Class<?>> getClasses(String packageName) {
+        List<Class<?>> classes = new ArrayList<>();
+        try {
+            String packagePath = packageName.replace('.', '/');
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+            Enumeration<URL> resources = classLoader.getResources(packagePath);
+
+            while (resources.hasMoreElements()) {
+                URL resource = resources.nextElement();
+                if (resource.getProtocol().equals("file")) {
+                    File directory = new File(resource.getFile());
+                    File[] files = directory.listFiles();
+                    if (files != null) {
+                        for (File file : files) {
+                            if (file.isFile() && file.getName().endsWith(".class")) {
+                                String className = packageName + '.' + file.getName().substring(0, file.getName().length()-6);
+                                try {
+                                    Class<?> clazz = Class.forName(className);
+                                    classes.add(clazz);
+                                } catch (ClassNotFoundException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return classes;
+    }
+
+    public static void main(String[] args) {
+        String packageName = "Collection"; // Replace with the package name you want to scan
+        List<Class<?>> classes = getClasses(packageName);
+
+        for (Class<?> clazz : classes) {
+            System.out.println(clazz.getSimpleName());
+        }
+    }
+}
